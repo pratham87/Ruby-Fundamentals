7a849395c27144c7eecaebb08ed8e49e661afdee
Prathamesh Kawale <kawa5@live.com>
2017-06-25 21:13:15 -0700

    initial commit

---
+++ basics.rb
@@ -0,0 +1,12 @@
+require 'fileutils'
+
+# Moving file
+#FileUtils.mv('/Users/Prathamesh/Desktop/Ruby/test.txt', '/Users/Prathamesh/Desktop/')
+
+
+# Getting data from user and add them.
+print "Enter first number: "
+first_num = gets.to_i  #convert string to int  
+print "Enter first number: "
+second_num = gets.to_i  
+puts "Addition is: " + (first_num + second_num).to_s
